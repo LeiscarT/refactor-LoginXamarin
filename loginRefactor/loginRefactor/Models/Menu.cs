bb6b124980e957c036791bd5981defcf9d3145b0
LeiscarT <leiscartp29@gmail.com>
2021-08-25 21:04:19 -0400

    add home structure

---
+++ loginRefactor/loginRefactor/Models/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace loginRefactor.Models
+{
+    public class Menu : INotifyPropertyChanged
+    {
+        private string _title { get; set; }
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; OnPropertyChanged(nameof(Title)); }
+        }
+
+        private string _image { get; set; }
+        public string Image
+        {
+            get { return _image; }
+            set { _image = value; OnPropertyChanged(nameof(Image)); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ loginRefactor/loginRefactor/ViewModels/HomeViewModel.cs
@@ -0,0 +1,48 @@
+﻿using loginRefactor.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace loginRefactor.ViewModels
+{
+   public class HomeViewModel
+    {
+        public Menu Beach { get; set; } = new Menu();
+        public Menu Church { get; set; } = new Menu();
+        public Menu River { get; set; } = new Menu();
+        public Menu Park { get; set; } = new Menu();
+        public Menu Hotel { get; set; } = new Menu();
+        public Menu FoodTruck { get; set; } = new Menu();
+
+         ICommand Command { get; set; }
+
+        public HomeViewModel()
+        {
+            Command = new Command(OnInit);
+        } 
+
+        private void OnInit()
+        {
+            Beach.Title = "Beach";
+            Beach.Image = "/.beach.png";
+
+            Church.Title = "Church";
+            Church.Image = "/.church.png";
+
+            River.Title = "River";
+            River.Image = "/.river.png";
+
+            Park.Title = "Park";
+            Park.Image = "/.park.png";
+
+            Hotel.Title = "Hotel";
+            Hotel.Image = "/.hotel.png";
+
+            FoodTruck.Title = "Beach";
+            FoodTruck.Image = "/.beach.png";
+        }
+
+    }
+}
