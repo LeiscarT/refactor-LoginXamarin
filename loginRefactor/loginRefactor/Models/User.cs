ae5479c805c83ba1fef32d6fad7f38e11c515241
LeiscarT <leiscartp29@gmail.com>
2021-08-25 20:37:40 -0400

    add logic to register form

--- loginRefactor/loginRefactor/Models/User.cs
+++ loginRefactor/loginRefactor/Models/User.cs
@@ -7,10 +7,15 @@ namespace loginRefactor.Models
 {
     public class User : INotifyPropertyChanged
     {
+        private string _name { get; set; }
+        public string Name { get { return _name; } set { _name = value; OnPropertyChanged(nameof(Name)); } }
         private string _userName { get; set; }
         public string Username { get { return _userName; } set { _userName = value; OnPropertyChanged(nameof(Username)); } }
         private string _password { get; set; }
         public string Password { get { return _password; } set { _password = value; OnPropertyChanged(nameof(Password)); } }
+
+        private string _confirmPass { get; set; }
+        public string ConfirmPass { get { return _confirmPass; } set { _confirmPass = value; OnPropertyChanged(nameof(ConfirmPass)); } }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
--- loginRefactor/loginRefactor/ViewModels/RegisterViewModel.cs
+++ loginRefactor/loginRefactor/ViewModels/RegisterViewModel.cs
@@ -1,14 +1,43 @@
-﻿using System;
+﻿using loginRefactor.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace loginRefactor.ViewModels
 {
-    public class RegisterViewModel
+    public class RegisterViewModel : INotifyPropertyChanged
     {
+        public User User { get; set; } = new User();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand Command { get; }
         public RegisterViewModel()
         {
-          
+            Command = new Command(OnPush);
         }
+
+        public async void OnPush()
+        {
+            
+            if(User.Password != User.ConfirmPass){
+                await App.Current.MainPage.DisplayAlert("Error", "No ha colocado la contraseña correcta", "Ok");
+            }
+
+            else if (string.IsNullOrEmpty(User.Username) || string.IsNullOrEmpty(User.Password) || string.IsNullOrEmpty(User.Name) || string.IsNullOrEmpty(User.ConfirmPass))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Debe llenar todos los campos", "Ok");
+
+            }
+            else 
+            {
+                await App.Current.MainPage.DisplayAlert("Success", "Usuario Creado", "Ok");
+                await App.Current.MainPage.Navigation.PopModalAsync();
+            }
+        }
+            
+        
     }
 }
